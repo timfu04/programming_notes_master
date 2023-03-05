10b9832a40d145663c613d9eacbf26eebb24c14b
Clement <clementlee1999@gmail.com>
2023-03-05 17:02:35 +0800

    completed react tutorial 22

---
+++ JavaScript/Full Modern React Tutorial - The Net Ninja/tutorial_22_exact_match_routes/dojo-blog/src/App.js
@@ -0,0 +1,25 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import Create from './Create';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
+function App() {  
+  return (
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/create' element={<Create/>}></Route>  
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+
