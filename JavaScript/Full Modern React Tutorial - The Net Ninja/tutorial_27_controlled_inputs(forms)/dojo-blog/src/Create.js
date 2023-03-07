2bd5250472332999d5f3d553ba7fd1674aa545d3
Lee Tim Fu <clement.lee@my.corp.seagroup.com>
2023-03-07 15:54:01 +0800

    completed react tutorial 27

---
+++ JavaScript/Full Modern React Tutorial - The Net Ninja/tutorial_27_controlled_inputs(forms)/dojo-blog/src/Create.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+    return (
+        <div className="create">
+            <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                {/* Controller input using React state */}
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                {/* Controller input using React state */}
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                {/* Controller input using React state */}
+                <select
+                    value = {author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
+        </div>
+    );
+}
+
+export default Create;
+
+
