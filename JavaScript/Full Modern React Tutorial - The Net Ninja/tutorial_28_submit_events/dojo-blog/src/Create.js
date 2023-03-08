8a845e965adb434807b759038f79315b7e987457
Lee Tim Fu <clement.lee@my.corp.seagroup.com>
2023-03-08 11:16:32 +0800

    completed react tutorial 28

---
+++ JavaScript/Full Modern React Tutorial - The Net Ninja/tutorial_28_submit_events/dojo-blog/src/Create.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevent the page refreshing after submit event
+        const blog = {title, body, author}; // JSON Server will automatically add "id" after POST request
+        console.log(blog);
+    };
+
+    return (
+        <div className="create">
+            <h2>Add a New Blog</h2>
+
+            {/* Call handleSubmit function after submit event */}
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value = {author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
+        </div>
+    );
+}
+
+export default Create;
+
+
