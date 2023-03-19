c33d1f47da36e7d6af854900da7ef2daba75d6c0
Clement <clementlee1999@gmail.com>
2023-03-19 16:37:51 +0800

    add java

---
+++ Java/Java Full Course - Bro Code/02.Variables/src/Main.java
@@ -0,0 +1,39 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x; // declaration
+		
+		x = 123; // assignment
+		
+		int y = 456; // initialization
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println("My number is: " + x);
+		
+		// add "L" suffix for number with long data type
+		long z = 123209384092830423L;
+		
+		// add "f" suffix for decimal numbers with float data type
+		float a = 3.14f;
+		
+		double b = 1.2163712637868762;
+		
+		boolean c = false;
+		
+		char symbol = '@';
+		
+		String name = "Bro";
+		
+		System.out.println(z);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(symbol);
+		System.out.println("Hello" + name);
+	}
+}
+
+
