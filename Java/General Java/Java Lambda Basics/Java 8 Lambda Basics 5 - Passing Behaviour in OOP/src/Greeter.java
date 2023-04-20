ecaba8f1baf35aa4357a85985d5dfea28486efa5
Clement <clementlee1999@gmail.com>
2023-04-20 21:07:49 +0800

    added java lambda tutorial 5

---
+++ Java/General Java/Java Lambda Basics/Java 8 Lambda Basics 5 - Passing Behaviour in OOP/src/Greeter.java
@@ -0,0 +1,14 @@
+public class Greeter {
+
+    // polymorphism
+    public void greet(Greeting greeting){
+        greeting.perform();
+    }
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
+        greeter.greet(helloWorldGreeting);
+    }
+
+}
---
+++ Java/General Java/Java Lambda Basics/Java 8 Lambda Basics 5 - Passing Behaviour in OOP/src/HelloWorldGreeting.java
@@ -0,0 +1,7 @@
+public class HelloWorldGreeting implements Greeting{
+
+    @Override
+    public void perform() {
+        System.out.println("Hello World Greeting!");
+    }
+}
